bf24fa23d05b4d50cbb5863f501c27800d8768c9
Breison Mendez <breisonmendez78@gmail.com>
2024-06-26 19:02:39 -0500

    Creacion de tema de tailwind y creacion de form Se crea el formulario de busqueda Se crea una clase en el tema de tailwind

--- src/views/Header.tsx
+++ src/views/Header.tsx
@@ -1,12 +1,44 @@
-import { useMemo } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { NavLink, useLocation } from "react-router-dom";
+import { useAppStore } from "../stores/useAppStore";
 
 export default function Header(){
 
+    const [ searchFilters , setSearchFilters ] = useState({
+        ingredient: '',
+        category:''
+    })
     const { pathname } = useLocation();//Saber la ubicacion de la pagina actual activa
     const isHome = useMemo( () => pathname === '/' , [ pathname ]);
+    const fetchCategories = useAppStore( ( state ) => state.fetchCategories );
+    const categories = useAppStore( ( state ) => state.categories );
+    const searchRecipies = useAppStore( ( state ) => state.searchRecipies );
+    useEffect( ( ) => { 
+        fetchCategories()
+    } , [ ])
+
+    const handleChangeEvent = ( e : React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLSelectElement> ) => { 
+        setSearchFilters({
+            ...searchFilters,
+            [ e.target.name ]: e.target.value
+        })
+    }
+
+    const onSubmit = ( e : React.FormEvent<HTMLFormElement>) => { 
+        e.preventDefault();
+        //TODO: Validar
+        if( Object.values( searchFilters ).includes('')){
+            console.log('Todos los campos son obligatorios')
+            return
+        }
+        //Consultar la receta
+        searchRecipies( searchFilters );
+
+
+    }
+
     return (
-        <header className="bg-slate-800">
+        <header className={ isHome ? 'bg-header bg-center bg-cover' : "bg-slate-800"}>
             <div className="mx-auto container px-5 py-16">
                 <div className="flex justify-between items-center">
                     <div className="">
@@ -30,10 +62,11 @@ export default function Header(){
                 </div>
                 { isHome && ( 
                     <form
+                        onSubmit={ onSubmit }
                         className="md:w-1/2 2xl:w-1/3 bg-orange-400 my-32 p-10 rounded-lg shadow space-y-6" >
                             <div className="space-y-4">
 
-                                <label htmlFor=""
+                                <label htmlFor="ingredient"
                                         className="block text-white uppercase font-extrabold text-lg"> 
                                 Nombre o Ingredientes</label>
                                 <input 
@@ -41,20 +74,30 @@ export default function Header(){
                                     type="text"
                                     name="ingredient"
                                     className="p-3 w-full rounded-lg focus:outline-none"
+                                    onChange={ handleChangeEvent }
+                                    value={ searchFilters.ingredient }
                                     placeholder="Nombre o Ingrediente. Ej. Vodkal, Tequila, Café" />
                             </div>
 
                             <div className="space-y-4">
 
-                                <label htmlFor=""
+                                <label htmlFor="category"
                                         className="block text-white uppercase font-extrabold text-lg"> 
                                 Categoria</label>
                                 <select
-                                    id="ingredient"
-                                    name="ingredient"
+                                    id="category"
+                                    name="category"
+                                    onChange={ handleChangeEvent }
+                                    value={ searchFilters.category }
                                     className="p-3 w-full rounded-lg focus:outline-none"
                                 >
                                   <option value="">-- Seleccione --</option>  
+                                  {categories.drinks.map( category => ( 
+                                    <option 
+                                            key={ category.strCategory }
+                                            value={ category.strCategory }>
+                                    { category.strCategory }</option>
+                                  ) ) }
                                 </select>
                             </div>
                             <input 
