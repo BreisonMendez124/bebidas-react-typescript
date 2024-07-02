991d83a2067768b551ed9bfef3eec9c68031cf30
Breison Mendez <breisonmendez78@gmail.com>
2024-07-02 16:52:54 -0500

    Agregar y eliminar de favoritos las recetas Creacion de slice pattern Saber como traer funciones de otros slice Creacion del slice de favoritos Agregar funciones del slice a los botones del Modal Guardar informacion en el local storage

--- src/stores/useAppStore.ts
+++ src/stores/useAppStore.ts
@@ -1,8 +1,10 @@
 import { create } from "zustand";
 import { RecipesSliceType, createRecipesSlice } from "./recipeSlice";
 import { devtools } from "zustand/middleware";
+import { FavoritesSliceType , createFavoritesSlice } from './favoritesSlice'
 
 
-export const useAppStore = create<RecipesSliceType>()(devtools( (...a) => ({ 
-    ...createRecipesSlice(...a)    
+export const useAppStore = create<RecipesSliceType & FavoritesSliceType >()(devtools( (...a) => ({ 
+    ...createRecipesSlice(...a),
+    ...createFavoritesSlice(...a)    
 })))
